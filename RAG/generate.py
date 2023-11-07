8f08fca22044bdfaee0e6f62094e7b5d8a3c7c4e
HRItdy <bittdy@gmail.com>
2023-11-07 13:14:51 +0700

    generate retrieve dataset

--- RAG/generate.py
+++ RAG/generate.py
@@ -1,22 +1,51 @@
 ## These tasks are used to train the embedding model and should be stored in '../GA/tasks.txt'.
-file_path = "GA/tasks.txt"
+##TODO: load from config
+LTL_OPS = ['A', 'O', 'N', 'G', 'U', 'X', 'E']
+APs = ['a', 'b', 'c', 'd', 'e', 'f']
+
+file_path = "../GA/normal_task.txt"
 with open(file_path) as f:
-    tasks = f.readlines()
+    tasks = f.read().splitlines()
 print(tasks[0])
 print(len(tasks))
 f.close()
 
-## Convert the prefix format of these tasks into the original format
-def parse(task):
-    return task
+## Preprocess the tasks.
+## Remove the AP-repeated tasks.
+import difflib
+def compare_tasks(task_a, task_b):
+    # Return true if two tasks are only different in atomic propositions
+    d = difflib.Differ()
+    diffs = list(d.compare(task_a, task_b))
+    for diff in diffs:
+        if diff.startswith('+') or diff.startswith('-'):
+            if diff[-1] in LTL_OPS:
+                return False
+    return True
+
+def onlyAP(task, record_task): 
+    # Return true if some task only AP different has been added in list
+    for record in record_task:
+        if compare_tasks(task, record):
+            return True
+    return False
+
+from tqdm import tqdm
+print('Preprocessing tasks...')
+record_task = [tasks[0]]
+for i in tqdm(range(len(tasks))):
+    if onlyAP(tasks[i], record_task):
+        continue
+    else:
+        record_task.append(tasks[i])     
 
 import json, openai
 # You need to set your OPENAI API key here
 # https://beta.openai.com/account/api-keys
 #openai.api_key = "TO_BE_SET"
-openai.api_key = "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"
+openai.api_key = "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"
 
-PROMPT = """Given a linear temporal logic task {ltlt}, assume this task doesn't satisfy the expected behavior, specify 10 similar tasks with corresponding behaviors, and analyse the mistake of {ltlt}. The following is an example, the subsequent output should have the same output format without other extra explaination.
+PROMPT = """Given a linear temporal logic task {ltlt}, assume this task doesn't satisfy the expected behavior, specify 5 similar tasks with corresponding behaviors, and analyse the mistake of {ltlt}. The following is an example, the subsequent output should have the same output format without other explaination.
 
 Task: 
 F(a U (b A X c))
@@ -61,7 +90,7 @@ Outputs:
 def rephrase_a_sentence(task):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=PROMPT.replace("TASK-TO-BE-PLACED", parse(task)),
+        prompt=PROMPT.replace("TASK-TO-BE-PLACED", task),
         temperature=0.7,
         max_tokens=512,
         top_p=1,
@@ -71,35 +100,56 @@ def rephrase_a_sentence(task):
         )
     output = response['choices'][0]['text']
     try:
-        paraphrases = generate_response(output)
+        mistakes, revised = generate_response(output)
     except:
         print("Error in parsing response")
         print(output)
         return output, "ERROR"
+    #print(mistakes, revised)
     return generate_response(output)
 
 def generate_response(response):
     lines = response.split('\n')
     print(lines[0])
     response = {}
+    mistakes = []
+    revised = []
     for idx, line in enumerate(lines):
         if line.startswith('Possible mistake:'):
-            mistakes.append(line.lstrip('Possible mistake:').lstrip().rstrip())
+            mistakes.append(line.replace('Possible mistake:', '').lstrip().rstrip())
         elif line.startswith('Revised task:'):
-            revised.append(line.lstrip('Revised task:').lstrip().rstrip())
+            revised.append(line.replace('Revised task:', '').lstrip().rstrip())
         else:
             continue
-        assert len(mistakes) != len(revised), 'The number of mistakes and revised tasks should be the same!'
+    assert len(mistakes) == len(revised), 'The number of mistakes and revised tasks should be the same!'
     return mistakes, revised
 
 info = {}
 def add_dict(task, mistakes, revised):
-    for i, mistake in enumerate(mistakes):
-        info[task][mistake] = revised[i]
+    info[task] = {}
+    if len(mistakes) == len(revised):
+        for i, mistake in enumerate(mistakes):
+            info[task][mistake] = revised[i]
+    else:
+        print('Length of mistakes and revised are not equal!')
+        r = min(len(mistakes), len(revised))
+        for i in range(r):
+            info[task][mistakes[i]] = revised[i]
 
-for task in tasks:
+print('Begin to generate responses...')
+from tqdm import tqdm
+# for i in tqdm(range(59, int(len(record_task)/10))):
+for i in tqdm(range(60, int(len(record_task))), leave=None):
+    task = record_task[i].strip()
     mistakes, revised = rephrase_a_sentence(task)
     add_dict(task, mistakes, revised)
 
-with open("sample_file.json", "w") as file:
-    json.dump(info, file)
+with open("retrieve_msg.json", "w") as file:
+    json.dump(info, file, indent=2)
+
+file.close()
+
+# with open('retrieve_msg.json') as json_file:
+#     data = json.load(json_file)
+
+# print('a')

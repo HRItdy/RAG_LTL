88c01fdbcf759b021401362ffe8b89cd698ae79d
HRItdy <bittdy@gmail.com>
2023-12-06 20:19:31 +0800

    generate code

--- evaluator.py
+++ evaluator.py
@@ -8,37 +8,22 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
 from ltl_utils import LTL
 import numpy as np
+import random
 from utils import *
 import networkx as nx
 
 DEVICE = "cuda" if th.cuda.is_available() else "cpu"
-## Initialize. load the retrieve dataset and get the embedding
-env = LTLBootcamp()
-model = PPO(CustomActorCriticPolicy, env, verbose=1, tensorboard_log="./tensorboard", device=DEVICE)
 
-# Load pre-trained weights for the LTL module
-model.policy.mlp_extractor.ltl_embedder.load_state_dict(th.load("./pre_logs/weights_ltl.pt"))
-model.policy.mlp_extractor.ga.load_state_dict(th.load("./pre_logs/weights_ga.pt"))
-# Get the embedding of the generated task
-# Load the retrieve dataset
 import json
 with open('./Retrieve/retrieve_msg.json') as retrieve_data:
     retrieve_dict = json.load(retrieve_data)
 
-embedding = []
-tasks_r = []
-for task_r in retrieve_dict.keys():
-    emb = get_task_embedding(task_r)
-    embedding.append(emb)
-    tasks_r.append(task_r)
-    
-## Input the prompt, and the task to be generated
-nl_task = input("Specify the task:\n")
-GEN_PROMPT = generate_prompt(nl_task)
+task_spec = random.choice(retrieve_dict.keys())
+GEN_PROMPT = generate_prompt(task_spec)
 ## Get the result
-task = get_response(nl_task, GEN_PROMPT)[0]
+task = get_response(task_spec, GEN_PROMPT)[0]
 
-## Step 1: Evaluate the generated task 
+# Evaluate the generated task 
 # ltl2ba, generate the policy sketch
 violation = True
 times = 0
